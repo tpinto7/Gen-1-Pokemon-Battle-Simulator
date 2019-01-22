2c8f622a8296777e6d4098222fed92ceeff3ce77
tpinto7 <43225579+tpinto7@users.noreply.github.com>
2019-01-22 09:12:18 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using System;
 using static System.Console;
 using System.Collections;
 using System.Collections.Generic;
 using PokemonTypes;
 using PokemonMoves;
+using Pokemons;
+using ActualMoves;
 
 namespace PokemonBattle
 {
@@ -15,20 +17,20 @@ namespace PokemonBattle
             
             while( true )
             {
-				Write( "Please enter the number of Pokemon you would like each trainer to battle with: " );
-				string response = ReadLine( );
-				WriteLine( );
-				
-				if( int.TryParse( response, out int val ) )
+			Write( "Please enter the number of Pokemon you would like each trainer to battle with: " );
+			string response = ReadLine( );
+			WriteLine( );
+
+			if( int.TryParse( response, out int val ) )
+			{
+				if( val >= 1 && val <= 6 )
 				{
-					if( val >= 1 && val <= 6 )
-					{
-						myBattle.NumberOfPokemon = val;
-						break;
-					}
+					myBattle.NumberOfPokemon = val;
+					break;
 				}
-				Write( "Pokemon should be between 1 and 6. (inclusive) " );
 			}
+			Write( "Pokemon should be between 1 and 6. (inclusive) " );
+	    }
             
             myBattle.CreatePlayers( );
             
@@ -436,7 +438,8 @@ namespace PokemonBattle
 		public int SpAttackStage{ get; set; }
 		public int SpDefenseStage{ get; set; }
 		public int SpeedStage{ get; set; }
-		public int AccuracyStage{ get; set; } // when moves with accuracy...
+		public int AccuracyStage{ get; set; }
+		public int EvasionStage{ get; set; }
 		public int AttackingTurnsConfused{ get; set; }
 		
 		public double CurrentSpeed{ get { return Speed * statMultipliers[ SpeedStage ]; } }
@@ -470,7 +473,7 @@ namespace PokemonBattle
 		
 		public void ResetPokemon( )
 		{
-			AttackStage = DefenseStage = SpAttackStage = SpDefenseStage = SpeedStage = AccuracyStage = 6;
+			AttackStage = DefenseStage = SpAttackStage = SpDefenseStage = SpeedStage = AccuracyStage = EvasionStage = 6;
 		}
 		
 		public Pokemon PokemonCreator( string playerName, int i )
@@ -509,30 +512,46 @@ namespace PokemonBattle
 					indexToSkip.Add( nextIndex );
 					switch( nextIndex )
 					{
+						case 1 : return new Bulbasaur( );
 						case 3 : return new Venusaur( );
+						case 4 : return new Charmander( );
 						case 6 : return new Charizard( );
+						case 7 : return new Squirtle( );
 						case 9 : return new Blastoise( );
 						case 16 : return new Pidgey( );
 						case 25 : return new Pikachu( );
+						case 26 : return new Raichu( );
 						case 34 : return new Nidoking( );
+						case 37 : return new Vulpix( );
+						case 38 : return new Ninetales( );
 						case 56 : return new Mankey( );
 						case 59 : return new Arcanine( );
 						case 65 : return new Alakazam( );
 						case 76 : return new Golem( );
-						case 80 : return new Rhydon( );
+						case 80 : return new Slowbro( );
 						case 85 : return new Dodrio( );
+						case 87 : return new Dewgong( );
 						case 91 : return new Cloyster( );
 						case 94 : return new Gengar( );
 						case 103 : return new Exeggutor( );
 						case 108 : return new Lickitung( );
+						case 112 : return new Rhydon( );
 						case 113 : return new Chansey( );
+						case 121 : return new Starmie( );
+						case 123 : return new Scyther( );
+						case 124 : return new Jynx( );
+						case 128 : return new Tauros( );
 						case 129 : return new Magikarp( );
 						case 130 : return new Gyarados( );
 						case 131 : return new Lapras( );
+						case 134 : return new Vaporeon( );
+						case 135 : return new Jolteon( );
+						case 136 : return new Flareon( );
 						case 143 : return new Snorlax( );
 						case 144 : return new Articuno( );
 						case 145 : return new Moltres( );
 						case 146 : return new Zapdos( );
+						case 147 : return new Dratini( );
 						case 149 : return new Dragonite( );
 						case 150 : return new Mewtwo( );
 						case 151 : return new Mew( );
@@ -717,151 +736,6 @@ namespace PokemonBattle
 			
 			return false;
 		}
-		//IEnumerable<Type> subclasses = types.Where( t=> t.BaseType == parentType );
-	}
-	// Healths are doubled
-	class Charizard : Pokemon 
-	{
-		public Charizard( ) : base( 6, "Charizard", new Types("Fire"), new Types("Flying"), 90, 78, 84, 78, 85, 85, 100, new Moves[ 4 ]{ new FlameThrower( ), new BellyDrum( ), new Roost( ), new DragonClaw( ) } ){ } 
 	}
 	
-	class Venusaur : Pokemon
-	{
-		public Venusaur( ) : base( 3, "Venusaur", new Types("Grass"), new Types("Poison"), 90, 80, 82, 83, 100, 100, 80, new Moves[ 4 ]{ new LeechSeed( ), new SolarBeam( ), new Toxic( ), new RazorLeaf( ) } ){ }
-	}
-	
-	class Blastoise : Pokemon
-	{
-		public Blastoise( ) : base( 9, "Blastoise", new Types("Water"), null, 90, 79, 83, 100, 85, 85, 78,  new Moves[ 4 ]{ new Harden( ), new HydroPump( ), new Rest( ), new IceBeam( ) } ){ }
-	}
-	
-	class Pidgey : Pokemon
-	{
-		public Pidgey( ) : base( 16, "Pidgey", new Types("Flying"), new Types("Normal"), 100, 40, 45, 40, 35, 35, 56, new Moves[ 4 ] { new Fly( ), new QuickAttack( ), new SandAttack( ), new Whirlwind( ) } ){ }
-	}
-	
-	class Pikachu : Pokemon 
-	{
-		public Pikachu( ) : base( 25, "Pikachu", new Types("Electric"), null, 100, 35, 55, 30, 50, 50, 90, new Moves[ 4 ]{ new Thunder( ), new ThunderWave( ), new IronTail( ), new LightScreen( ) } ){ }
-	}
-	
-	class Nidoking : Pokemon
-	{
-		public Nidoking( ) : base( 34, "Nidoking", new Types("Ground"), new Types("Poison"), 84, 81, 92, 77, 75, 75, 85, new Moves[ 4 ]{ new Fissure( ), new Earthquake( ), new Thunderbolt( ), new Blizzard( ) } ){ }
-	}
-	
-	class Mankey : Pokemon
-	{
-		public Mankey( ) : base( 56, "Mankey", new Types("Fighting"), null, 100, 40, 80, 35, 35, 35, 70, new Moves[ 4 ] { new SeismicToss( ), new Dig( ), new Submission( ), new Thunder( ) } ){ }
-	}
-	
-	class Arcanine : Pokemon
-	{
-		public Arcanine( ) : base( 59, "Arcanine", new Types("Fire"), null, 80, 90, 110, 80, 80, 80, 95, new Moves[ 4 ]{ new FireBlast( ), new BodySlam( ), new Mimic( ), new Reflect( ) } ) { }
-	}
-	
-	class Alakazam : Pokemon
-	{
-		public Alakazam( ) : base( 65, "Alakazam", new Types("Psychic"), null, 80, 55, 50, 45, 135, 135, 120, new Moves[ 4 ]{ new Psychic( ), new Recover( ), new Reflect( ), new ThunderWave( ) } ){ }
-	}
-	
-	class Golem : Pokemon
-	{
-		public Golem( ) : base( 76, "Golem", new Types("Ground"), new Types("Rock"), 80, 80, 110, 130, 55, 55, 45, new Moves[ 4 ]{ new BodySlam( ), new Earthquake( ), new RockSlide( ), new Explosion( ) } ){ }
-	}
-	
-	class Rhydon : Pokemon
-	{
-		public Rhydon( ) : base( 80, "Rhydon", new Types("Ground"), new Types("Rock"), 80, 105, 130, 120, 45, 45, 40, new Moves[ 4 ]{ new Earthquake( ), new RockSlide( ), new Substitute( ), new BodySlam( ) } ){ }
-	}
-	
-	class Dodrio : Pokemon
-	{
-		public Dodrio( ) : base( 85, "Dodrio", new Types("Normal"), new Types("Flying"), 84, 60, 110, 70, 60, 60, 100, new Moves[ 4 ]{ new DrillPeck( ), new Mimic( ), new HyperBeam( ), new Toxic( ) } ){ }			
-	}
-	
-	class Cloyster : Pokemon
-	{
-		public Cloyster( ) : base( 91, "Cloyster", new Types("Water"), new Types("Ice"), 80, 50, 95, 180, 85, 85, 70, new Moves[ 4 ]{ new Explosion( ), new Clamp( ), new Blizzard( ), new Surf( ) } ){ }
-	}
-	
-	class Gengar : Pokemon
-	{
-		public Gengar( ) : base( 94, "Gengar", new Types("Ghost"), new Types("Poison"), 80, 60, 65, 60, 130, 130, 110, new Moves[ 4 ]{ new Hypnosis( ), new MegaDrain( ), new Thunderbolt( ), new Explosion( ) } ){ } 
-	}
-	
-	class Exeggutor : Pokemon
-	{
-		public Exeggutor( ) : base( 103, "Exuggutor", new Types("Grass"), new Types("Psychic"), 80, 95, 95, 85, 125, 125, 55, new Moves[ 4 ]{ new SleepPowder( ), new Psychic( ), new Explosion( ), new MegaDrain( ) } ){ } 
-	}
-	
-	class Lickitung : Pokemon
-	{
-		public Lickitung( ) : base( 108, "Lickitung", new Types("Normal"), null, 80, 90, 55, 75, 60, 60, 30, new Moves[ 4 ]{ new Fissure( ), new MegaKick( ), new Rest( ), new Supersonic( ) } ){ } 
-	}
-	
-	class Chansey : Pokemon
-	{
-		public Chansey( ) : base( 113, "Chansey", new Types("Normal"), null, 74, 250, 5, 5, 105, 105, 50, new Moves[ 4 ]{ new SoftBoiled( ), new IceBeam( ), new ThunderWave( ), new Thunderbolt( ) } ){ }
-	}
-	
-	class Scyther : Pokemon
-	{
-		public Scyther( ) : base( 123, "Scyther", new Types("Bug"), new Types("Flying"), 84, 70, 110, 80, 55, 55, 105, new Moves[ 4 ]{ new Slash( ), new SwordsDance( ), new Agility( ), new HyperBeam( ) } ){ }
-	}
-	
-	class Magikarp : Pokemon
-	{
-		public Magikarp( ) : base( 129, "Magikarp", new Types("Water"), null, 100, 20, 10, 55, 20, 20, 80, new Moves[ 3 ]{ new DragonRage( ), new Splash( ), new Tackle( ) } ){ }
-	}
-	
-	class Gyarados : Pokemon
-	{
-		public Gyarados( ) : base( 130, "Gyarados", new Types("Water"), new Types("Flying"), 84, 95, 125, 79, 100, 100, 81, new Moves[ 4 ]{ new Blizzard( ), new Thunderbolt( ), new Surf( ), new HyperBeam( ) } ){ }
-	}
-	
-	class Lapras : Pokemon
-	{
-		public Lapras( ) : base( 131, "Lapras", new Types("Ice"), new Types("Water"), 80, 130, 85, 80, 95, 95, 60, new Moves[ 4 ]{ new IceBeam( ), new Thunderbolt( ), new Rest( ), new ConfuseRay( ) } ){ }
-	}
-	
-	/*class Jolteon : Pokemon
-	{
-		public Jolteon( ) : base( 135, "Jolteon", new Types("Electric"), null, 80, 65, 65, 60, 110, 110, 130, new Moves[ 4 ]{ new Thunderbolt( ), new ThunderWave( ), new PinMissile( ), new SandAttack( ) } ){ }
-	}*/
-	
-	class Snorlax : Pokemon
-	{
-		public Snorlax( ) : base( 143, "Snorlax", new Types("Normal"), null, 78, 160, 110, 65, 65, 65, 30, new Moves[ 4 ]{ new BodySlam( ), new HyperBeam( ), new Earthquake( ), new SelfDestruct( ) } ) { }
-	}
-	
-	class Articuno : Pokemon
-	{
-		public Articuno( ) : base( 144, "Articuno", new Types("Ice"), new Types("Flying"), 80, 90, 85, 100, 125, 125, 85, new Moves[ 4 ]{ new Blizzard( ), new Agility( ), new Rest( ), new HyperBeam( ) } ) { }
-	}
-	
-	class Zapdos : Pokemon
-	{
-		public Zapdos( ) : base( 145, "Zapdos", new Types("Electric"), new Types("Flying"), 80, 90, 90, 85, 125, 125, 100, new Moves[ 4 ]{ new Thunderbolt( ), new DrillPeck( ), new ThunderWave( ), new Agility( ) } ) { }
-	}
-	
-	class Moltres : Pokemon 
-	{
-		public Moltres( ) : base( 146, "Moltres", new Types("Fire"), new Types("Flying"), 80, 90, 100, 90, 125, 125, 90, new Moves[ 4 ]{ new Agility( ), new FireBlast( ), new FireSpin( ), new HyperBeam( ) } ) { }
-	}
-	
-	class Dragonite : Pokemon
-	{
-		public Dragonite( ) : base( 149, "Dragonite", new Types("Dragon"), new Types("Flying"), 84, 91, 134, 95, 100, 100, 80,  new Moves[ 4 ]{ new Agility( ), new Wrap( ), new HyperBeam( ), new Blizzard( ) } ) { }
-	}
-	
-	class Mewtwo : Pokemon
-	{
-		public Mewtwo( ) : base( 150, "Mewtwo", new Types("Psychic"), null, 68 ,106, 110, 90, 154, 154, 130,  new Moves[ 4 ]{ new Amnesia( ), new Psychic( ), new IceBeam( ), new Recover( ) } ) { }
-	}
-	
-	class Mew : Pokemon
-	{
-		public Mew( ) : base( 151, "Mew", new Types("Psychic"), null, 72, 100, 100, 100, 100, 100, 100, new Moves[ 4 ]{ new SwordsDance( ), new Earthquake( ), new HyperBeam( ), new SoftBoiled( ) } ) { }
-	}
 }
